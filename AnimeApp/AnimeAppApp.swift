a1446bdae409d2fc896a10eec680c45c41a1c8a9
phonwalai123 <phonwalai123@gmail.com>
2021-06-21 16:34:37 +0700

    Initial Commit

---
+++ AnimeApp/AnimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeAppApp.swift
+//  AnimeApp
+//
+//  Created by phonwalai on 21/6/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct AnimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
